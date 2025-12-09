6c26dc7f00ea876f40db4667f677839cf485dbc2
Yashveer singh <yashveersingh1921@gmail.com>
2025-12-09 23:28:44 +0530

    home route completed started with the coin detail route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { fetchCrypto } from "../api/coinGecko";
 import CryptoCard from "../components/CryptoCard";
 const Home = () => {
-    const [cyrptoList, setCryptoList] = useState([]);
+    const [cryptoList, setCryptoList] = useState([]);
+    const [filteredList, setFilteredList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [viewMode, setViewMode] = useState('grid');
+    const [sortBy, setSortBy] = useState('market_cap_rank');
+    const [searchQuery, setSearchQuery] = useState('')
     const fetchCryptoData = async () =>{
         try{
             const data = await fetchCrypto();
@@ -18,12 +21,53 @@ const Home = () => {
     useEffect(() => {
         fetchCryptoData();
     }, []);
+    
+    useEffect(() => {
+        filterAndSort();
+    }, [cryptoList, sortBy, searchQuery]);
+    const filterAndSort = () => {
+        let filtered = cryptoList.filter((crypto)=>
+            crypto.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            crypto.symbol.toLowerCase().includes(searchQuery.toLowerCase()));
+        filtered.sort((a, b) => {
+            switch(sortBy) {
+            case 'name':
+                return a.name.localeCompare(b.name);
+            case 'price':
+                return a.current_price-b.current_price;
+            case 'price_desc':
+                return b.current_price-a.current_price;
+            case 'change':
+                return a.price_change_percentage_24h-b.price_change_percentage_24h;
+            case 'market_cap':
+                return a.market_cap-b.market_cap;
+            default:
+                return a.market_cap_rank-b.market_cap_rank;
+            }
+        });
+        setFilteredList(filtered);
+    };
     return (
         <div className="app">
+            <header className="header">
+                <div className="header-content">
+                    <div className="logo-section">
+                        <h1>🚀 Crypto Tracker</h1>
+                        <p>Real-time Cryptocurrency prices and market data</p>
+                    </div>
+                    <div className="search-section">
+                        <input type="text" 
+                               onChange={(e)=>{setSearchQuery(e.target.value)}} 
+                               value={searchQuery}
+                               placeholder="Search Cryptos..."
+                               className="search-input" />
+                    </div>
+                </div>
+            </header>
             <div className="controls">
                 <div className="filter-group">
                     <label>Sort By:</label>
-                    <select>
+                    <select value={sortBy} onChange={(e)=>{setSortBy(e.target.value)}}>
                         <option value="market_cap_rank">Rank</option>
                         <option value="name">Name</option>
                         <option value="price">Price(low to high)</option>
@@ -46,7 +90,7 @@ const Home = () => {
                 </div>
             ) : (
                 <div className={`crypto-container ${viewMode}`}>
-                    {cyrptoList.map((crypto, key) => (
+                    {filteredList.map((crypto, key) => (
                         <CryptoCard crypto={crypto} key={key} />
                     ))}
                 </div>
